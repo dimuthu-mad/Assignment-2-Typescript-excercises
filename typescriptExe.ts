ba750ebab1bbc484d6bce288062dda826791a4b6
Dimuthu <dimuthumaduranga96@gmail.com>
2026-02-01 23:38:27 +0100

    add enums

--- typescriptExe.ts
+++ typescriptExe.ts
@@ -62,7 +62,6 @@ const printTeacherInfo  = (teacher: SchoolTeacher) => {
 }
 
 printTeacherInfo({name: "Dimuthu ", subject: "Math", id: 11, email:"dimuthu@gmail.com"});
-*/
 
 // Exercise 3
 interface Car {
@@ -74,3 +73,63 @@ const printCar = (car: Car) => {
     console.log("Brand: " + car.brand + "\nYear: " + car.year);
 }
 printCar({brand: "Audi RS6", year: "2025"});
+
+//3. Enums (fixed list of options)  
+// Exercise 1
+
+enum Color {
+    red = "RED",
+    green = "GREEN",
+    blue = "BLUE" ,  
+}
+const showColor = (color: Color) => {
+    if(color === Color.red){
+        console.log("You chose"+ " "+ Color.red); 
+    }else if(color === Color.green){
+        console.log("You chose"+" "+  Color.green);
+    }else if(color === Color.blue){
+        console.log("You chose"+" "+  Color.blue);
+    }
+}
+showColor(Color.red);
+showColor(Color.green);
+showColor(Color.blue);
+
+
+// Exercise 2
+enum PizzaSize {
+    small = "SMALL",
+    medium = "MEDIUM",
+    large = "LARGE",
+}
+const orderPizza = (size: PizzaSize) => {
+    if(size === PizzaSize.small){
+    console.log(`You ordered a ${size} pizza.`);
+}else if(size === PizzaSize.medium){
+        console.log(`You ordered a ${size} pizza.`);
+    }else if(size === PizzaSize.large){
+        console.log(`You ordered a ${size} pizza.`);
+    }
+}
+
+orderPizza(PizzaSize.large);
+*/
+
+// Exercise 3
+enum Role {
+    admin = "Admin",
+    user = "User",
+    guest = "Guest",
+}
+const printRole = (role: Role) => {
+    if(role === Role.admin){
+        console.log("You have full access");
+    }else if(role === Role.user){
+        console.log("You have limited access");
+    }else if(role === Role.guest){
+        console.log("You have guest access");
+    }
+}
+printRole(Role.admin);
+printRole(Role.user);
+printRole(Role.guest);
